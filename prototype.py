e78259f236db048d1be5ab5ae1920f8d0fcc2840
Bryaan159 <00057720@uca.edu.sv>
2023-12-09 00:04:19 -0600

    I deleted 2 files but I created a enviroment for chatService :hammer:

---
+++ prototype.py
@@ -0,0 +1,12 @@
+import  nltk 
+from nltk.stem.lancaster import LancasterStemmer
+
+stemmer  = LancasterStemmer()
+import numpy
+import tflearn
+import tensorflow
+import json
+import random
+import pickle
+
+nltk.download('punkt')
