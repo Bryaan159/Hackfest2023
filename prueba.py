6eb6d22e1a9ac0f41c0d61ea8a3143bee7100e98
Bryaan159 <00057720@uca.edu.sv>
2023-12-08 22:45:09 -0600

    Se hace el primer commit de CS :fire:

---
+++ prueba.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+raiz  = Tk()
+raiz.title("Ventana de pruebas")
+raiz.resizable(True,True)
+raiz.config(bg="Blue",bd=10,relief="groove",cursor="hand1")
+
+myFrame = Frame(raiz, width="450",height="650",bg="green",relief="sunken",)
