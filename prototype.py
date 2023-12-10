99e088e516ffd9ac1be65cabf6acd8591122471f
Sirenoman <00003120@uca.edu.sv>
2023-12-09 19:29:48 -0600

    :rocket: add prototype chatbot

--- prototype.py
+++ prototype.py
@@ -4,11 +4,10 @@ from nltk.stem.lancaster import LancasterStemmer
 stemmer  = LancasterStemmer()
 import numpy
 import tflearn
-import tensorflow
+import tensorflow as tf
 import json
 import random
 import pickle
-
 
 with open("contentJson.json") as file:
     data = json.load(file)
